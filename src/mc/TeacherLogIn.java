e6d4cb1aee827f262cf6b45c60558294f3088140
Redowanur Rahman <69495647+Redowanur@users.noreply.github.com>
2022-08-04 19:44:32 +0600

    Update TeacherLogIn.java

--- src/mc/TeacherLogIn.java
+++ src/mc/TeacherLogIn.java
@@ -1,22 +1,29 @@
 package mc;
 
+import com.k33ptoo.components.KButton;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.event.MouseInputListener;
 
 import keeptoo.KGradientPanel;
 
 import javax.swing.*;
 
-public class TeacherLogIn extends JFrame implements MouseInputListener{
+public class TeacherLogIn extends JFrame implements MouseInputListener {
+
     javax.swing.JPanel Background;
     javax.swing.JPanel LeftPanel;
     javax.swing.JPanel RightPanel;
     KGradientPanel TitleBar;
     javax.swing.JLabel Welcome;
-    javax.swing.JButton jButton1;
+    KButton jButton1, kButton4;
     javax.swing.JCheckBox jCheckBox2;
     javax.swing.JLabel jLabel1;
     javax.swing.JLabel jLabel2;
@@ -68,7 +75,7 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         label5.setIcon(scaledIcon5);
         label5.addMouseListener(this);
         TitleBar.add(label5);
-        
+
         a = new JLabel();
         a.setBounds(5, 6, 15, 15);
         ImageIcon icon9 = new ImageIcon("src/mc/assests/cross1.png");
@@ -78,7 +85,7 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         a.setIcon(scaledIcon9);
         a.addMouseListener(this);
         TitleBar.add(a);
-        
+
         label6 = new JLabel();
         label6.setBounds(25, 6, 15, 15);
         label6.addMouseListener(this);
@@ -88,7 +95,7 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         ImageIcon scaledIcon6 = new ImageIcon(imgScale6);
         label6.setIcon(scaledIcon6);
         TitleBar.add(label6);
-        
+
         label7 = new JLabel();
         label7.setBounds(45, 6, 15, 15);
         label7.addMouseListener(this);
@@ -98,7 +105,7 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         ImageIcon scaledIcon7 = new ImageIcon(imgScale7);
         label7.setIcon(scaledIcon7);
         TitleBar.add(label7);
-        
+
         b = new JLabel();
         b.setBounds(45, 6, 15, 15);
         b.addMouseListener(this);
@@ -147,19 +154,52 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         jPasswordField1.setBorder(javax.swing.BorderFactory.createEmptyBorder());
         jPasswordField1.setCaretColor(new java.awt.Color(255, 255, 255));
         jPasswordField1.setBounds(162, 347, 195, 41);
+        jPasswordField1.setEchoChar('\u25CF');
         //jPasswordField1.add(jLabel1);
 
-        jButton1 = new JButton();
-        jButton1.setBackground(new java.awt.Color(105, 247, 39));
-        jButton1.setFont(new java.awt.Font("Avenir LT Std 55 Roman", 1, 24)); // NOI18N
+        jButton1 = new KButton();
+        jButton1.setBorder(null);
+        jButton1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 16));
+        jButton1.setkBorderRadius(40);
         jButton1.setText("Sign In");
         jButton1.setAlignmentX(0.5F);
-        jButton1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(51, 255, 51), new java.awt.Color(51, 255, 51), new java.awt.Color(51, 255, 51), new java.awt.Color(51, 255, 51)));
         jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jButton1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
         jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jButton1.setBounds(120, 440, 260, 45);
+        jButton1.setkEndColor(new java.awt.Color(57, 187, 12));
+        jButton1.setkHoverEndColor(new java.awt.Color(105, 247, 39));
+        jButton1.setkHoverForeGround(new java.awt.Color(255, 255, 255));
+        jButton1.setkHoverStartColor(new java.awt.Color(57, 187, 12));
+        jButton1.setkStartColor(new java.awt.Color(105, 247, 39));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jButton1.setFocusable(false);
+
+        kButton4 = new KButton();
+        kButton4.setBorder(null);
+        kButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mc/assests/go_back.png"))); // NOI18N
+        kButton4.setText("Go Back");
+        kButton4.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
+        kButton4.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        kButton4.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        kButton4.setkBorderRadius(20);
+        kButton4.setkEndColor(new java.awt.Color(33, 47, 61));
+        kButton4.setkForeGround(new java.awt.Color(0, 0, 0));
+        kButton4.setkHoverEndColor(new java.awt.Color(23, 32, 42));
+        kButton4.setkHoverForeGround(new java.awt.Color(207, 207, 207));
+        kButton4.setkHoverStartColor(new java.awt.Color(33, 47, 61));
+        kButton4.setkStartColor(new java.awt.Color(23, 32, 42));
+        kButton4.setBounds(25, 573, 120, 30);
+        kButton4.setkForeGround(new java.awt.Color(207, 207, 207));
+        kButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kButton4ActionPerformed(evt);
+            }
+        });
 
         jLabel3 = new JLabel();
         jLabel3.setFont(new java.awt.Font("Segoe UI Semibold", 0, 16)); // NOI18N
@@ -184,8 +224,13 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         jCheckBox2.setText("Show Password");
         jCheckBox2.setBorder(null);
         jCheckBox2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jCheckBox2.setBounds(190, 406, 113, 21);
+        jCheckBox2.setBounds(190, 406, 130, 21);
         jCheckBox2.setFocusable(false);
+        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox2ActionPerformed(evt);
+            }
+        });
 
         RightPanel.add(jTextField1);
         RightPanel.add(jPasswordField1);
@@ -225,6 +270,7 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         LeftPanel.add(jLabel6);
         LeftPanel.add(Welcome);
         LeftPanel.add(jLabel7);
+        LeftPanel.add(kButton4);
         LeftPanel.setLocation(0, 27);
 
         Background.add(LeftPanel);
@@ -233,80 +279,133 @@ public class TeacherLogIn extends JFrame implements MouseInputListener{
         //this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
-    
-    private void TitleBarMousePressed(java.awt.event.MouseEvent evt) {                                      
+
+    private void TitleBarMousePressed(java.awt.event.MouseEvent evt) {
         xx = evt.getX();
         yy = evt.getY();
-    }                                     
+    }
 
-    private void TitleBarMouseDragged(java.awt.event.MouseEvent evt) {                                      
+    private void TitleBarMouseDragged(java.awt.event.MouseEvent evt) {
         int x = evt.getXOnScreen();
         int y = evt.getYOnScreen();
         this.setLocation(x - xx, y - yy);
-    } 
-    // public static void main(String[] args) {
-    //     new TeacherLogIn();
-    // }
+    }
+
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TeacherDashBoard2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TeacherDashBoard2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TeacherDashBoard2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TeacherDashBoard2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        new TeacherLogIn().setVisible(true);
+    }
+    
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt){
+        if(jCheckBox2.isSelected()) jPasswordField1.setEchoChar((char)0);
+        else jPasswordField1.setEchoChar('\u25CF');
+    }
+    
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizhub", "root", "connected");
+            Statement st = con.createStatement();
+            String query = "select * from teacher where id='" + jTextField1.getText() + "' and password='" + jPasswordField1.getText().toString() + "'";
+            ResultSet rs = st.executeQuery(query);
+            if (rs.next()) {
+                TeacherDashBoard2 t = new TeacherDashBoard2();
+                t.setVisible(true);
+                String id = rs.getNString("id");
+                String name = rs.getNString("name");
+                t.updateID(id);
+                dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Incorrect username and password...");
+            }
+            con.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void kButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        HomePage hm = new HomePage();
+        hm.setVisible(true);
+        hm.setLocation(this.getX(), this.getY());
+        dispose();
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getSource() == a) {
-			System.exit(0);
-		}
-		if(e.getSource() == b) {
-			this.setState(this.ICONIFIED);
-		}
+        if (e.getSource() == a) {
+            System.exit(0);
+        }
+        if (e.getSource() == b) {
+            this.setState(this.ICONIFIED);
+        }
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        if(e.getSource() == label5) {
-			label5.setVisible(false);
-			a.setVisible(true);
-			a.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		}
-		if(e.getSource() == label7) {
-			label7.setVisible(false);
-			b.setVisible(true);
-			b.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		}
+        if (e.getSource() == label5) {
+            label5.setVisible(false);
+            a.setVisible(true);
+            a.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
+        if (e.getSource() == label7) {
+            label7.setVisible(false);
+            b.setVisible(true);
+            b.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        if(e.getSource() == a) {
-			a.setVisible(false);
-			label5.setVisible(true);
-			label5.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		}
-		if(e.getSource() == b) {
-			b.setVisible(false);
-			label7.setVisible(true);
-			label7.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		}  
+        if (e.getSource() == a) {
+            a.setVisible(false);
+            label5.setVisible(true);
+            label5.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
+        if (e.getSource() == b) {
+            b.setVisible(false);
+            label7.setVisible(true);
+            label7.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
         // TODO Auto-generated method stub
-        
+
     }
 }
